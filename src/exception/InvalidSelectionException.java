327fbcd90a1d139bdc07f682cf7c9e5d38f5d4c2
naaznabiha <naaznabiha41@gmail.com>
2025-03-04 22:55:45 +0200

    added the exception package

---
+++ src/exception/InvalidSelectionException.java
@@ -0,0 +1,14 @@
+package exception;
+
+abstract public class InvalidSelectionException extends GameException {
+	
+	public InvalidSelectionException() {
+		super();
+	}
+	
+	public InvalidSelectionException(String message) {
+		super(message);
+	}
+
+
+}

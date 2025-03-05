797e22050be4639bfaf37cb9d1dcd7d9458dd82f
naaznabiha <naaznabiha41@gmail.com>
2025-03-05 21:06:19 +0200

    added the game class after getting rid of errors

---
+++ src/engine/Game.java
@@ -0,0 +1,54 @@
+package engine;
+
+import model.player.Player;
+import model.player.CPU;
+import model.card.Card;
+import model.card.Deck;
+import engine.board.Board;
+import engine.board.BoardManager;
+import model.Colour;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Game implements GameManager {
+    private final Board board;
+    private final ArrayList<Player> players;
+    private final ArrayList<Card> firePit;
+    private int currentPlayerIndex;
+    private int turn;
+
+    public Game(String playerName) throws IOException {
+        // Shuffle colors for random player assignments
+        ArrayList<Colour> colourOrder = new ArrayList<>();
+        Collections.addAll(colourOrder, Colour.RED, Colour.GREEN, Colour.BLUE, Colour.YELLOW);
+        Collections.shuffle(colourOrder, new Random());
+
+        // Initialize game board
+        this.board = new Board(colourOrder , this);
+
+        // Load card pool
+        Deck.loadCardPool((BoardManager) this.board, this);
+
+        // Initialize players
+        this.players = new ArrayList<>();
+        players.add(new Player(playerName, colourOrder.get(0))); // Human player
+
+        // Add CPU players
+        for (int i = 1; i < 4; i++) {
+            players.add(new CPU("CPU " + i, colourOrder.get(i), (BoardManager) this.board));
+        }
+
+        // Distribute initial cards
+        for (Player player : players) {
+            player.getHand().addAll(Deck.drawCards());
+        }
+
+        // Initialize turn tracking
+        this.currentPlayerIndex = 0;
+        this.turn = 0;
+        this.firePit = new ArrayList<>();
+    }
+}

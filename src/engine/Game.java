78375a5ca88cdd75c10bac964f834987b6c5b12c
naaznabiha <naaznabiha41@gmail.com>
2025-04-18 18:40:42 +0200

    deleted jackaroom1 and updated orignal file

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,68 +1,60 @@
 package engine;
 
-import model.player.Player;
-import model.player.CPU;
-import model.card.Card;
-import model.card.Deck;
-import engine.board.Board;
-import engine.board.BoardManager;
-import model.Colour;
-
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.Random;
 
+import engine.board.Board;
+import model.Colour;
+import model.card.Card;
+import model.card.Deck;
+import model.player.*;
+
+@SuppressWarnings("unused")
 public class Game implements GameManager {
     private final Board board;
     private final ArrayList<Player> players;
+	private int currentPlayerIndex;
     private final ArrayList<Card> firePit;
-    private int currentPlayerIndex;
     private int turn;
 
     public Game(String playerName) throws IOException {
-        // Shuffle colors for random player assignments
+        turn = 0;
+        currentPlayerIndex = 0;
+        firePit = new ArrayList<>();
+
         ArrayList<Colour> colourOrder = new ArrayList<>();
-        Collections.addAll(colourOrder, Colour.RED, Colour.GREEN, Colour.BLUE, Colour.YELLOW);
+        
+        colourOrder.addAll(Arrays.asList(Colour.values()));
+        
         Collections.shuffle(colourOrder);
-
-        // Initialise game board
-        this.board = new Board(colourOrder , this);
-
-        // Load card pool
-        Deck.loadCardPool((BoardManager) this.board, this);
-
-        // Initialise players
+        
+        this.board = new Board(colourOrder, this);
+        
+        Deck.loadCardPool(this.board, (GameManager)this);
+        
         this.players = new ArrayList<>();
-        players.add(new Player(playerName, colourOrder.get(0))); // Human player
-
-        // Add CPU players
-        for (int i = 1; i < 4; i++) {
-            players.add(new CPU("CPU " + i, colourOrder.get(i), (BoardManager) this.board));
-        }
-
-        // Distribute initial cards
-        for (Player player : players) {
-            player.setHand(Deck.drawCards());
-        }
-
-        // Initialise turn tracking
-        this.currentPlayerIndex = 0;
-        this.turn = 0;
-        this.firePit = new ArrayList<>();
+        this.players.add(new Player(playerName, colourOrder.get(0)));
+        
+        for (int i = 1; i < 4; i++) 
+            this.players.add(new CPU("CPU " + i, colourOrder.get(i), this.board));
+        
+        for (int i = 0; i < 4; i++) 
+            this.players.get(i).setHand(Deck.drawCards());
+        
+    }
+    
+    public Board getBoard() {
+        return board;
     }
 
-	public Board getBoard() {
-		return board;
-	}
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
 
-	public ArrayList<Player> getPlayers() {
-		return players;
-	}
-
-	public ArrayList<Card> getFirePit() {
-		return firePit;
-	}
-    
+    public ArrayList<Card> getFirePit() {
+        return firePit;
+    }
     
 }

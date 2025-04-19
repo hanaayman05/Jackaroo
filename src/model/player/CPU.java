1d7a085ac6b0989e9b4f0984f698145ff8465df5
naaznabiha <naaznabiha41@gmail.com>
2025-04-19 22:16:59 +0200

    Hana's part and CPU class update

--- src/model/player/CPU.java
+++ src/model/player/CPU.java
@@ -2,6 +2,11 @@ package model.player;
 import engine.board.*;
 import engine.*;
 import model.*;
+import model.card.Card;
+import exception.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
 public class CPU extends Player {
     private final BoardManager boardManager;
 
@@ -13,4 +18,60 @@ public class CPU extends Player {
     public BoardManager getBoardManager() {
         return boardManager;
     }
+    
+    @Override
+    public void play() throws InvalidCardException {
+        ArrayList<Marble> actionableMarbles = boardManager.getActionableMarbles();
+        ArrayList<Card> handCards = new ArrayList<>(getHand());
+        Collections.shuffle(handCards);
+
+        for (Card card : handCards) {
+            try {
+                
+                try {
+                    selectCard(card);
+                    card.act(new ArrayList<>());
+                    return;
+                } catch (Exception e) {}
+
+                
+                Collections.shuffle(actionableMarbles);
+                for (Marble marble : actionableMarbles) {
+                    try {
+                        selectCard(card);
+                        selectMarble(marble);
+                        card.act(new ArrayList<>(Collections.singletonList(marble)));
+                        return;
+                    } catch (Exception e) {}
+                }
+
+                
+                if (actionableMarbles.size() >= 2) {
+                    for (int i = 0; i < actionableMarbles.size(); i++) {
+                        for (int j = i+1; j < actionableMarbles.size(); j++) {
+                            try {
+                                selectCard(card);
+                                selectMarble(actionableMarbles.get(i));
+                                selectMarble(actionableMarbles.get(j));
+                                ArrayList<Marble> selectedPair = new ArrayList<>();
+                                selectedPair.add(actionableMarbles.get(i));
+                                selectedPair.add(actionableMarbles.get(j));
+                                card.act(selectedPair);
+                                return;
+                            } catch (Exception e) {}
+                        }
+                    }
+                }
+            } finally {
+                deselectAll();
+            }
+        }
+
+        
+        if (!handCards.isEmpty()) {
+            selectCard(handCards.get(0));
+        }
+    }
+    
+    
 }
